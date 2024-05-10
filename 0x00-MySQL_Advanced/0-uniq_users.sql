ea0e9029cebedfe741ce65a9df4460b7d469c08c
Oyadier Robert Mensah <oyadierrobertmensah@gmail.com>
2024-05-10 04:05:41 +0000

    We are all unique!

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,5 +1,5 @@
 --create a users table--
 CREATE TABLE IF NOT EXIST users (
-    id INT PRIMARY_KEY AUTO_INCREMENT,
-    email VARCHAR(255) NOT NULL Unique,
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255));
