a9a6ea40d8612ff4ce69f1ba605d8c671492a56b
Oyadier Robert Mensah <oyadierrobertmensah@gmail.com>
2024-05-10 06:00:39 +0000

    Buy buy buy

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -2,8 +2,6 @@
 CREATE Triggers qty_decrease
 AFTER INSERT ON order_times
 FOR EACH ROWS
-BEGIN
-    UPDATE items
-    SET quantity = quantity - NEW.qty_order
-    WHERE item_id = NEW.item_id
-END;
+UPDATE items
+SET quantity = quantity - NEW.number
+WHERE name = NEW.item_name;
