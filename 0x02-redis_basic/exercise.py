fe6c84f54d9cd2fa286e211053690d9b64f5e036
Oyadier Robert Mensah <oyadierrobertmensah@gmail.com>
2024-05-16 05:49:23 +0000

    Writing string

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -6,12 +6,19 @@ from typing import Union
 
 
 class Cache():
+    """
+        Cache class.
+    """
 
     def __init__(self):
+        '''
+            Initialize the cache class
+        '''
         self._redis = redis.Redis()
         self._redis.flushdb
 
     def store(self, data: Union[str, int, float, bytes]) -> str:
+        ''' Store data in the cache '''
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
